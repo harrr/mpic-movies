e0ff2c5da9841313b760a3a0234bce7aa3e8ae52
harrr <alinaami@mail.ru>
2014-04-15 00:49:05 +0400

    preference algo created

--- algo/src/main/java/algo/PreferencesAlgo.java
+++ algo/src/main/java/algo/PreferencesAlgo.java
@@ -3,6 +3,7 @@ package algo;
 import models.Movie;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by vitaly on 4/9/14.
@@ -11,6 +12,13 @@ public class PreferencesAlgo {
 
     public Movie getForRating(List<Movie> candidates, List<Movie> watched) {
         //TODO: get random movie by from non watched candidates
-        return null;
+        for(Movie movie:watched){
+            int iWatched = candidates.indexOf(movie);
+            if(iWatched>=0)
+                candidates.remove(iWatched);
+        }
+        Random random = new Random();
+        int randomNum = random.nextInt(candidates.size());
+        return candidates.get(randomNum);
     }
 }
